08a4517bc1a203d8cfd6530be6da5d413a625851
iyu.swiftLeaner <iyu@live.jp>
2021-05-12 21:20:10 +0900

    カテゴリーで”全て”を選択するように変更

---
+++ taskapp/Task.swift
@@ -0,0 +1,34 @@
+//
+//  Task.swift
+//  taskapp
+//
+//  Created by 岩渕優児 on 2021/04/28.
+//
+
+import RealmSwift
+
+class Task: Object {
+    //管理用ID。プライマリーキー
+    @objc dynamic var id = 0
+    
+    //タイトル
+    @objc dynamic var title = ""
+    
+    //カテゴリー
+    @objc dynamic var category: Category?
+    
+    //内容
+    @objc dynamic var contents = ""
+    
+    //日時
+    @objc dynamic var date = Date()
+    
+    //idをプライマリーキーとして設定
+    override static func primaryKey() -> String? {
+        return "id"
+    }
+
+}
+    
+
+
